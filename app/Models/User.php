f69ade4df0a6f5a176e220af0301538c8f00d220
yakudzaa4ever <yakudzaax@gmail.com>
2025-02-19 01:21:11 +0500

    Updated UserController

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,41 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'password'];
 
     public function posts()
     {
         return $this->hasMany(Post::class);
-    }
-
-
-
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
     }
 }
