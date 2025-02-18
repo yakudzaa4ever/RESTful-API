015e9ab87fdbde6d953506efeee259881c0bf436
yakudzaa4ever <yakudzaax@gmail.com>
2025-02-18 21:43:28 +0500

    Added UpdatePostRequest for updating posts

---
+++ app/Http/Requests/UpdatePostRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true; 
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|string|max:255',
+            'content' => 'sometimes|string',
+            'user_id' => 'required|exists:users,id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.string' => 'Sarlavha matn bo‘lishi kerak.',
+            'content.string' => 'Kontent matn bo‘lishi kerak.',
+            'user_id.required' => 'User ID talab qilinadi.',
+            'user_id.exists' => 'Ushbu User ID mavjud emas.'
+        ];
+    }
+}
