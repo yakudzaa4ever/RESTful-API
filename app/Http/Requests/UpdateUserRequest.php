590500c1335a3eb75471f395cdc7a7be6ab48990
yakudzaa4ever <yakudzaax@gmail.com>
2025-03-27 23:50:55 +0500

    deleted

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
+
 class UpdateUserRequest extends FormRequest
 {
     public function authorize(): bool
